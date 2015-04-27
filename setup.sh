476f2a91e54f475afc77e2547ae57aa343fd916e
seanstrom <seanstrom@seanstroms-MacBook-Pro.local>
2015-04-26 18:15:01 -0700

    add more modules

--- setup.sh
+++ setup.sh
@@ -1,2 +1,16 @@
+mkdir -p ~/.dotmods
+
 git clone git@github.com:DotMods/vim.git ~/.dotmods/vim
 ~/.dotmods/vim/setup.sh
+
+git clone git@github.com:DotMods/bash.git ~/.dotmods/bash
+~/.dotmods/bash/setup.sh
+
+git clone git@github.com:DotMods/tmux.git ~/.dotmods/tmux
+~/.dotmods/tmux/setup.sh
+
+git clone git@github.com:DotMods/fish.git ~/.dotmods/fish
+~/.dotmods/fish/setup.sh
+
+git clone git@github.com:DotMods/git.git ~/.dotmods/git
+~/.dotmods/git/setup.sh
