c16302f3acab4244672131ddd3e7c347f7b0987f
seanstrom <sean.hagstrom@originate.com>
2015-06-09 20:54:52 -0700

    patches

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ git clone git@github.com:DotMods/vim.git ~/.dotmods/vim
 git clone git@github.com:DotMods/bash.git ~/.dotmods/bash
 ~/.dotmods/bash/setup.sh
 
+git clone git@github.com:DotMods/zsh.git ~/.dotmods/zsh
+~/.dotmods/zsh/setup.sh
+
 git clone git@github.com:DotMods/tmux.git ~/.dotmods/tmux
 ~/.dotmods/tmux/setup.sh
 
@@ -14,3 +17,6 @@ git clone git@github.com:DotMods/fish.git ~/.dotmods/fish
 
 git clone git@github.com:DotMods/git.git ~/.dotmods/git
 ~/.dotmods/git/setup.sh
+
+git clone git@github.com:DotMods/scripts.git ~/.dotmods/scripts
+~/.dotmods/scripts/setup.sh
